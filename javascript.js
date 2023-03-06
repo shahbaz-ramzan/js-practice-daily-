ef1c9b210813ed222dc822bbcf8f8ade9ab71e90
shahbaz ramzan <mshahbazramzan952@gmail.com>
2023-03-06 11:58:55 +0500

    march 6

---
+++ javascript.js
@@ -0,0 +1,284 @@
+// function findMax(arr) {
+//     let max = arr[0]; // set the maximum to the first element
+  
+//     for (let i = 1; i < arr.length; i++) {
+//       if (arr[i] > max) {
+//         max = arr[i]; // update the maximum if the current element is larger
+//       }
+//     }
+  
+//     return max;
+//   }
+
+//   let arr=[1,2,3,3,4,5,6];
+//   let maximum=findMax(arr);
+//   console.log(maximum)
+
+
+// let arr = [1,2,3,4,5,6];
+// let maximum=Math.max(...arr);
+// console.log(maximum);
+
+
+// function findMax(arr){
+//     let max = arr[0];
+//     for(let i = 0;i<arr.length;i++){
+//         if(max<arr[i]){
+//             max=arr[i];
+//         }
+        
+
+//     }
+//     return max;
+// }
+// let arr= [22,21,33,31,44,41,55];
+// let newMax= findMax(arr);
+// console.log(newMax)
+
+
+// function findMax(arr){
+//     console.log(arr)
+//     let max= Math.max(...arr)
+//     console.log(max)
+// }
+// let arr =[1,2,3,4,5,6,7,8,9,0];
+// findMax(arr);
+
+
+
+// function findMin(arr){
+//     let min=arr[0];
+//     for(let i = 0;i<arr.length;i++){
+//         if(arr[i]<min){
+//             min=arr[i]
+//         }
+//     }
+//     return min;
+// }
+
+
+// let arr=[98,5,4,88,1];
+// let minimum = findMin(arr);
+// console.log(minimum);
+
+// function findMax(array){
+//     let max= array[0];
+//     for(let i =0;i<array.length;i++){
+//         if(array[i]>max){
+//             max=array[i];
+//         }
+//     }
+//     return max;
+// }
+
+
+
+
+
+// let array=[5555,1,656,976,888,0];
+// let maximum= findMax(array);
+// console.log(maximum)
+
+
+
+// let maxNum=Math.max(...array);
+// console.log(maxNum)
+
+// let minNum=Math.min(...array);
+// console.log(minNum)
+
+
+
+                          // odd number with for loop
+
+
+
+        // for(let i= 0;i<=30;i++){
+        //     if(i%2==1){
+        //         console.log(i)
+        //     }
+        // }
+
+
+
+        //write a program to print the sum of even number from 2 to 50
+
+
+        // let sum=0;
+        // for(let i=0;i<=50;i++){
+        //     if(i%2==0){
+        //         sum +=i;
+        //         console.log("sum=",sum)
+        //     }
+        // }
+        // console.log("GrandSum=",sum)
+
+
+        //write a program to print the sum of given series 1+2+3 to 49
+
+
+        // let sum = 0;
+        // for (let i =1;i<=49;i++){
+        //     sum +=i;
+        //     console.log(sum);
+        // }
+        // console.log("GrandSum=",sum)
+
+
+        // let array =[1,4,5,6,3,9];
+
+        // function findMax(array){
+        //     let max=array[0]
+        //     for(let i=0;i<array.length;i++){
+        //         if(array[i]>max){
+        //             max=array[i];
+                    
+        //         }
+        //     }
+        //     return max;
+        // }
+
+        // const maximum=findMax(array);
+        // console.log(maximum)
+
+
+        //find max value with the help of Math.max
+
+
+        // let array=[4,3,7,9,2]
+
+
+
+        // function findMax(array){
+        //     let max= Math.max(...array);
+        //     console.log(max)
+        // }
+        // findMax(array);
+
+
+        // let array=[1,2,3,4,5,6,7,8,55,44,33];
+
+        // function findMin(array){
+        //     let min = Math.min(...array);
+        //     console.log(min);
+        // }
+
+        // findMin(array)
+
+
+
+    //     let array = [3,4,6,8,9,2,1];
+
+    //     function findMin(array){
+    //         let min=array[0];
+    //         for(let i =0;i<array.length;i++){
+    //             if(array[i]<min){
+    //                 min=array[i]
+    //             }
+    //         }
+    //         return min;
+    //     }
+    // let minimum=    findMin(array)
+    //     console.log(minimum);
+
+
+
+    // let array=[1,2,3,4,5];
+
+    // function findMax(array){
+    //     let max = Math.max(...array)
+    //     return max
+    // }
+
+    // let maximum = findMax(array);
+    // console.log(maximum);
+
+
+                       //write a program that print the sum of series 1^2 2^2 3^2 .,...10^2
+
+
+
+    //   let sum = 0;
+      
+    //   for (i=0;i<=10;i++){
+    //     sum=sum+(i*i)
+    //     console.log("iteration sum",sum)
+    //   }
+    //   console.log(sum);
+
+
+
+
+//                    // write a program to print all two digit odd number and their sum
+// let sum =0;
+// for(let i=10;i<=99;i++){
+//     if(i%2==1){
+//         console.log(i)
+//         sum=sum+i;
+//     }
+// }
+// console.log(sum)
+
+                   // write a program that print all two digit even numbar and their sum
+
+
+
+
+// let sum =  0;
+// for(let i =10;i<=99;i++){
+//     if(i%2==0){
+//         console.log(i,"is even number");
+//         sum=sum+i;
+
+//     }
+
+// }
+// console.log(sum);
+
+
+
+                     // 27 feb
+
+
+
+    // let num1 = 10;
+    // let num2 =5;
+    // let sum =0
+    // for(let i=0;i<num2;i++){
+    //     sum =sum+num1;
+    // }
+    // console.log(sum)  
+
+
+
+                           //factorial
+
+
+
+
+
+        //     let num = 5;
+        // let fact=1;
+        // for(let i =1;i<=num;i++){
+        //     fact=fact*i;
+        // }
+
+
+        // console.log("factorial of num is " ,fact)
+
+
+
+
+        // create table for any number
+
+
+
+        // let num =10;
+        
+
+        // for(i=1;i<=10;i++){
+        //   let product = num*i;
+        //   console.log(`${num}*${i}=${product}`)  
+        // }
+
+    
